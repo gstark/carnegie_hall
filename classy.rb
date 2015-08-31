2f3c939b7cd742b1407599ad0b6eccb989b83b42
Gavin Stark <gavin@gstark.com>
2015-08-31 08:53:31 -0400

    Practice sample for classes and methods

---
+++ classy.rb
@@ -0,0 +1,13 @@
+
+class Human
+end
+
+
+# Make it so humans have a first name
+# Make it so humans have a last name
+# Make it so humans have a greeting of their first name followed by their last name
+# Make it so humans can buy some ounces of coffee
+# Make it so humans can say if they have ANY coffee
+# Make it so humans can drink a number of ounces of coffee they have, but not more than they have
+# Make it so humans are alert if the total amount of coffee they drank is more than 12 ounces
+# Make it so humans can report each amount of coffee they drank during the day
